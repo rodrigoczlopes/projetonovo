f013bfba2f1291b4e634b27f0d01559f996fbc44
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-06-21 11:11:05 -0300

    primeiro commit

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$adress;
+
+var user = {
+  name: 'Rodrigo'
+};
+console.log((_user$adress = user.adress) === null || _user$adress === void 0 ? void 0 : _user$adress.street);
